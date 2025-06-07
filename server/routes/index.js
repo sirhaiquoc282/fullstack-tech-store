5b2f54863aceae2b96b71dfcaa1ebfdb68303a51
tuananhdayne <117759723+tuananhdayne@users.noreply.github.com>
2025-06-08 00:11:37 +0700

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,13 @@
 const userRouter = require('./user');
+const productRouter = require('./product'); // Đã import đúng
+const orderRoutes = require('./order');
+
 const { notFound, errorHandler } = require('../middlewares/errorHandler');
 
 const initRoutes = (app) => {
-    app.use('/api/v1/user', userRouter);
-
-
-    app.use(notFound);
-    app.use(errorHandler);
+    app.use('/api/v1/user', userRouter);      // Route cho user
+    app.use('/api/v1/product', productRouter); // ✅ Route cho sản phẩm
+    app.use('/api/orders', orderRoutes);
+    app.use(notFound);     // Middleware xử lý route không tồn tại
+    app.use(errorHandler); // Middleware xử lý lỗi chung
 }
-
-module.exports = initRoutes;
